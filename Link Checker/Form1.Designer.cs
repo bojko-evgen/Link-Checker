c334e43dee9bcb4ae9729ece1d1fc839cb2020ae
dmitry369 <dmitry@producters.com>
2016-08-30 17:47:43 +0300

    Add project files.

---
+++ Link Checker/Form1.Designer.cs
@@ -0,0 +1,94 @@
+﻿namespace Link_Checker
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.LinksList = new System.Windows.Forms.ListBox();
+            this.InputTextbox = new System.Windows.Forms.TextBox();
+            this.InfoHelper = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // LinksList
+            // 
+            this.LinksList.AllowDrop = true;
+            this.LinksList.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.LinksList.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.LinksList.FormattingEnabled = true;
+            this.LinksList.Location = new System.Drawing.Point(12, 51);
+            this.LinksList.Name = "LinksList";
+            this.LinksList.SelectionMode = System.Windows.Forms.SelectionMode.MultiExtended;
+            this.LinksList.Size = new System.Drawing.Size(260, 197);
+            this.LinksList.TabIndex = 1;
+            this.LinksList.DoubleClick += new System.EventHandler(this.LinksList_DoubleClick);
+            this.LinksList.KeyDown += new System.Windows.Forms.KeyEventHandler(this.LinksList_KeyDown);
+            // 
+            // InputTextbox
+            // 
+            this.InputTextbox.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.InputTextbox.Location = new System.Drawing.Point(12, 12);
+            this.InputTextbox.Name = "InputTextbox";
+            this.InputTextbox.Size = new System.Drawing.Size(260, 20);
+            this.InputTextbox.TabIndex = 0;
+            this.InputTextbox.KeyDown += new System.Windows.Forms.KeyEventHandler(this.InputTextbox_KeyDown);
+            // 
+            // InfoHelper
+            // 
+            this.InfoHelper.AutoSize = true;
+            this.InfoHelper.Location = new System.Drawing.Point(13, 34);
+            this.InfoHelper.Name = "InfoHelper";
+            this.InfoHelper.Size = new System.Drawing.Size(0, 13);
+            this.InfoHelper.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.InfoHelper);
+            this.Controls.Add(this.InputTextbox);
+            this.Controls.Add(this.LinksList);
+            this.MinimumSize = new System.Drawing.Size(300, 300);
+            this.Name = "Form1";
+            this.Text = "Link checker";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox LinksList;
+        private System.Windows.Forms.TextBox InputTextbox;
+        private System.Windows.Forms.Label InfoHelper;
+    }
+}
+
---
+++ Link Checker/Form1.cs
@@ -0,0 +1,221 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Link_Checker
+{
+    public partial class Form1 : Form
+    {
+        private readonly String FileName = "LinksData.dat";
+
+        private enum Actions
+        {
+            Copy,
+            Paste,
+            Delete,
+            SelectAll,
+            DeselectAll,
+            Add
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            LoadItems();
+        }
+
+        // ---------------------methods-------------------------------
+        private void AddUniqueLink(String item = "")
+        {
+            var InputLink = String.Empty;
+            if (item != "")
+            {
+                InputLink = item.StartsWith("+")
+                    ? item.Remove(0, 1).Trim()
+                    : item.Trim();
+            }
+            else
+            {
+                InputLink = InputTextbox.Text.StartsWith("+")
+                    ? InputTextbox.Text.Remove(0, 1).Trim()
+                    : InputTextbox.Text.Trim();
+            }
+
+            var LinkItems = GetListData();
+
+            if (InputLink.Trim() != "" && !LinkItems.Contains(InputLink))
+            {
+                LinksList.Items.Add(InputLink.Replace("+ ", ""));
+
+            }
+        }
+
+        private List<String> GetListData()
+        {
+            return LinksList.Items.Cast<String>().Select(item => item.StartsWith("+") ? item.Remove(0, 1) : item).ToList();
+        }
+
+        private void ConvertLink()
+        {
+            LinksList.Items[LinksList.SelectedIndex] = LinksList.SelectedItem.ToString().StartsWith("+")
+                ? LinksList.SelectedItem.ToString().Replace("+", "").Trim()
+                : LinksList.SelectedItem.ToString().Insert(0, "+ ");
+        }
+
+        private String GetSelectedLink()
+        {
+            var q = LinksList.SelectedItem.ToString();
+            return q.StartsWith("+")
+                ? LinksList.SelectedItem.ToString().Replace("+", "").Trim()
+                : LinksList.SelectedItem.ToString();
+        }
+
+        private void LoadItems()
+        {
+            BinaryFormatter binFormat = new BinaryFormatter();
+            using (Stream fStream = new FileStream(FileName, FileMode.OpenOrCreate))
+            {
+                if (fStream.Length == 0) return;
+                LinksList.Items.Clear();
+                (binFormat.Deserialize(fStream) as List<String>).ForEach(item => LinksList.Items.Add(item));
+            }
+
+        }
+
+        private void SaveItems()
+        {
+            BinaryFormatter binFormat = new BinaryFormatter();
+            using (Stream fStream = new FileStream(FileName, FileMode.Create))
+            {
+                binFormat.Serialize(fStream, LinksList.Items.Cast<String>().ToList());
+            }
+        }
+
+        private void CopyItems()
+        {
+            if (LinksList.SelectedItem == null) return;
+            String Data = "";
+            LinksList.SelectedItems.Cast<String>().ToList().ForEach(item => Data += item.ToString() + Environment.NewLine);
+            Clipboard.SetData(DataFormats.Text, Data);
+        }
+
+        private void PasteItems()
+        {
+            Clipboard.GetData(DataFormats.Text).ToString().Split(Environment.NewLine.ToCharArray(), StringSplitOptions.RemoveEmptyEntries).ToList().ForEach(item => AddUniqueLink(item));
+        }
+
+        private void DeleteItems()
+        {
+            if (MessageBox.Show("Delete items?", "Warning", MessageBoxButtons.OKCancel) == DialogResult.OK)
+            {
+                LinksList.SelectedItems.Cast<String>().ToList().ForEach(item => LinksList.Items.Remove(item));
+            }
+        }
+
+        private void SelectAll(Boolean selectAll)
+        {
+            var index = 0;
+            LinksList.Items.Cast<String>().ToList().ForEach(item => LinksList.SetSelected(index++, selectAll));
+            InfoHelper.Text = "All items selected";
+        }
+
+        private void AddNewLink()
+        {
+            if (InputTextbox.Text.Length == 0) return;
+            AddUniqueLink();
+            InputTextbox.Clear();
+        }
+
+        private void ItemsActions(Actions action)
+        {
+            switch (action)
+            {
+                case Actions.Copy:
+                    {
+                        CopyItems();
+                        SelectAll(false);
+                        break;
+                    }
+                case Actions.Paste:
+                    {
+                        PasteItems();
+                        break;
+                    }
+                case Actions.Delete:
+                    {
+                        DeleteItems();
+                        break;
+                    }
+                case Actions.SelectAll:
+                    {
+                        SelectAll(true);
+                        break;
+                    }
+                case Actions.DeselectAll:
+                    {
+                        SelectAll(false);
+                        return;
+                    }
+                case Actions.Add:
+                    {
+                        AddNewLink();
+                        break;
+                    }
+                default:
+                    {
+                        return;
+                    }
+            }
+            SaveItems();
+        }
+
+        // ------------------------keys events-------------------------------
+        private void InputTextbox_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                ItemsActions(Actions.Add);
+            }
+        }
+
+        private void LinksList_DoubleClick(object sender, EventArgs e)
+        {
+            ConvertLink();
+            SaveItems();
+            Clipboard.SetData(DataFormats.Text, GetSelectedLink());
+        }
+
+        private void LinksList_KeyDown(object sender, KeyEventArgs e)
+        {
+            e.SuppressKeyPress = true;
+            if (e.Control && e.KeyCode == Keys.A)
+            {
+                ItemsActions(Actions.SelectAll);
+            }
+            if (e.Control && e.KeyCode == Keys.C)
+            {
+                ItemsActions(Actions.Copy);
+            }
+            if (e.Control && e.KeyCode == Keys.V)
+            {
+                ItemsActions(Actions.Paste);
+            }
+            if (e.KeyCode == Keys.Delete)
+            {
+                ItemsActions(Actions.Delete);
+            }
+        }
+    }
+}
